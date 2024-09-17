f547a40d2428661b409b63c06bc79d3d8703511e
ukasz Gil <lukasz.gil@4soft.co>
2024-09-17 14:08:44 +0200

    chore: remove node_modules from repo

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+const courses = [
+  { id: 1, name: "course1" },
+  { id: 2, name: "course2" },
+  { id: 3, name: "course3" },
+];
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.get("/api/courses", (req, res) => {
+  res.send(courses);
+});
+
+app.get("/api/courses/:id", (req, res) => {
+  const course = courses.find(
+    (course) => course.id === parseInt(req.params.id)
+  );
+  if (course === undefined) res.status(404).send("Not found");
+  res.send(course);
+});
+
+app.post("/api/courses", (req, res) => {
+  if (!req.body.name || req.body.name.length < 3) {
+    res.status(400).send("Name is required and should be minimum 3 characters");
+    return;
+  }
+
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name,
+  };
+  courses.push(course);
+  res.send(course);
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
