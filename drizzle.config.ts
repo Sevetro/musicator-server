b4f1bba2c29b9f6ba86295e3194f4871562f6e0d
ukasz Gil <lukasz.gil@4soft.co>
2024-09-19 17:28:54 +0200

    feat: setting up drizzle

---
+++ drizzle.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "drizzle-kit";
+
+export default defineConfig({
+  schema: "./src/drizzle/schema.ts",
+  out: "./src/drizzle/migrations",
+  dialect: "postgresql",
+  driver: "pglite",
+  dbCredentials: {
+    url: process.env.DATABASE_URL as string,
+  },
+  verbose: true,
+  strict: true,
+});
