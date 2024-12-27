9c5427c955890ee870494d14a86f2b285e143624
ukasz Gil <lukasz.gil@4soft.co>
2024-12-27 22:12:40 +0100

    try 1

--- src/index.ts
+++ src/index.ts
@@ -37,12 +37,12 @@ app.use(
   })
 );
 
-// app.options("/register", (req, res) => {
-//   res.set("Access-Control-Allow-Origin", "https://frontend.example.com");
-//   res.set("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
-//   res.set("Access-Control-Allow-Headers", "Content-Type, Authorization");
-//   res.send();
-// });
+app.options("/register", (req, res) => {
+  res.set("Access-Control-Allow-Origin", "https://frontend.example.com");
+  res.set("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.set("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.send();
+});
 
 app.get("/", async (req, res) => {
   console.log(`accessed /`);
