c7c94af4089675cde77ee274b5dd9783fab8d6e4
ukasz Gil <lukasz.gil@4soft.co>
2025-01-08 23:01:44 +0100

    remove logs

--- src/utils/send-confirmation-email.ts
+++ src/utils/send-confirmation-email.ts
@@ -3,9 +3,6 @@ import nodemailer from "nodemailer";
 import { localhostServerUrl } from "../constants/urls.ts";
 import { musicatorEmail, musicatorEmailPass } from "../constants/envs.ts";
 
-console.log(`musicatorEmail`, musicatorEmail);
-console.log(`musicatorEmailPass`, musicatorEmailPass);
-
 const transporter = nodemailer.createTransport({
   host: "smtp.wp.pl",
   port: 465,
