a684c793c3b86870ae1d35f3c98cfdbe37fc17ed
ukasz Gil <lukasz.gil@4soft.co>
2025-01-16 10:10:41 +0100

    correct cors

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import cors from "cors";
 
 import { logMiddleware } from "./middleware/log-middleware.ts";
 import { localhostAppUrl, musicatorAppUrl } from "./constants/urls.ts";
-import { MY_PRIVATE_IP, SERVER_PORT } from "./constants/envs.ts";
+import { SERVER_PORT } from "./constants/envs.ts";
 import { registerController } from "./controllers/register-controller.ts";
 import { confirmEmailController } from "./controllers/confirm-email-controller.ts";
 import { loginController } from "./controllers/login-controller.ts";
@@ -16,12 +16,7 @@ app.use("*", logMiddleware);
 
 app.use(
   cors({
-    // origin: [musicatorAppUrl, localhostAppUrl, MY_PRIVATE_IP],
-    // origin: [musicatorAppUrl, localhostAppUrl],
-    origin: [localhostAppUrl],
-
-    // origin: "*",
-
+    origin: [musicatorAppUrl, localhostAppUrl],
     methods: ["GET", "POST", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     // credentials: true, //TODO: to delete?
